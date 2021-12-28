9e52de66aa9c68f8f18a91fcbd3e1d26ce634f01
David256 <wasterdavid@gmail.com>
2021-12-28 12:54:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,17 @@
 
 import os
 from measurement import measurement
+from measurement.errors import MeasurementError, MeasurementUnhandledError
 
 dev_mode = os.environ.get('MEASUREMENT_ENV', 'production') == 'development'
+
+
+def print_result(success: bool, filename: str = None, e: Exception = None):
+    print('Estado =>', success)
+    if filename:
+        print('Ruta =>', filename)
+    if e:
+        print('Error =>', e)
 
 
 def main():
@@ -15,11 +24,19 @@ def main():
         source_path = input('Archivo fuente: ')
         destination_path = input('Archivo destino: ')
 
-    # Read data
-    m = measurement.Measurement(source_path, destination_path)
+    try:
+        # Read data
+        m = measurement.Measurement(source_path, destination_path)
 
-    # Save processed data
-    m.save()
+        # Save processed data
+        filename = m.save()
+
+        # Print results
+        print_result(True, filename=filename)
+    except MeasurementError as e:
+        print_result(False, e=e)
+    except Exception as e:
+        print_result(False, e=MeasurementUnhandledError(e))
 
 
 if __name__ == '__main__':
