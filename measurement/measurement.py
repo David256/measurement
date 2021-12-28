34671daf00972a38b7bcc3f882f46c4d619b3360
David256 <wasterdavid@gmail.com>
2021-12-28 12:53:37 -0500

    Update measurement with method to save

--- measurement/measurement.py
+++ measurement/measurement.py
@@ -4,20 +4,26 @@ import os
 
 from .record import MeasurementRecord
 from .data import MeasurementData
-from .errors import MeasurementFileNotFoundError, MeasurementRecordError
+from .errors import MeasurementFileNotFoundError, MeasurementRecordError, MeasurementUnhandledError
 from .errors import MeasurementDestinationNotFoundError
 from .log import logger
+
+import xlsxwriter
 
 
 class Measurement(object):
     def __init__(self, source_path: str, destination_path: str) -> None:
         object.__init__(self)
+        self.source_path = source_path
+        self.destination_path = destination_path
         self.records: list[MeasurementRecord] = []
-        if not os.path.exists(source_path):
-            raise MeasurementFileNotFoundError(source_path)
+        if not os.path.exists(self.source_path):
+            raise MeasurementFileNotFoundError(self.source_path)
+        if not os.path.exists(os.path.dirname(self.destination_path)):
+            raise MeasurementDestinationNotFoundError(self.destination_path)
 
         # Open the file and read
-        file = open(source_path, 'r')
+        file = open(self.source_path, 'r')
         line = file.readline()
         count = 0
 
@@ -30,7 +36,10 @@ class Measurement(object):
                 # Now read data to current record
                 line = file.readline()
                 while line != '':
-                    if MeasurementData.is_data(line):
+                    if (
+                       MeasurementData.is_data(line)
+                       and not MeasurementRecord.is_header(line)
+                       ):
                         count += 1
                         record << line
                         # Read next line
@@ -49,5 +58,57 @@ class Measurement(object):
                 raise MeasurementRecordError(line)
         file.close()
 
-    def save(self):
-        pass
+    def save(self) -> str:
+        date = self.records[0].date
+        date_str = date.strftime('%y%m%d')
+        filename = f'{self.destination_path}{date_str}.xls'
+        try:
+            workbook = xlsxwriter.Workbook(filename)
+            worksheet = workbook.add_worksheet()
+        except xlsxwriter.exceptions.XlsxFileError as e:  # type: ignore
+            raise MeasurementUnhandledError(e)
+
+        # Read each record
+        counter = 1
+        row = 0
+        added_header = False
+        for record in self.records:
+            # Reset the col value
+            col = 0
+
+            # If the header has not been added yet, then:
+            if not added_header:
+                worksheet.write(row, col, 'No')
+                col += 1
+                worksheet.write(row, col, 'Fecha')
+                for variable in record.variables:
+                    col += 1
+                    worksheet.write(row, col, f'Cabezote {variable.value}')
+
+                # Next state
+                added_header = True
+                col = 0
+
+            # Get the current date
+            current_date = record.date
+            # Read all data for current record
+            for data in record.data:
+                row += 1
+                # Write the index and the date
+                worksheet.write(row, col, counter)
+                col += 1
+                worksheet.write(row, col,
+                                current_date.strftime('%Y-%m-%d %H:%M:%S'))
+
+                for cell in data:
+                    col += 1
+                    worksheet.write(row, col, f'{cell:.3f}')
+                col = 0
+
+                # Update the next date
+                current_date += record.sample
+                # Increase the counter
+                counter += 1
+        # Close book
+        workbook.close()
+        return filename
