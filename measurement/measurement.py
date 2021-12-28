94353ea1e6f829cbd7d4036564376f9b0453c386
David256 <wasterdavid@gmail.com>
2021-12-28 11:23:00 -0500

    Update and fix measurement class

--- measurement/measurement.py
+++ measurement/measurement.py
@@ -6,6 +6,7 @@ from .record import MeasurementRecord
 from .data import MeasurementData
 from .errors import MeasurementFileNotFoundError, MeasurementRecordError
 from .errors import MeasurementDestinationNotFoundError
+from .log import logger
 
 
 class Measurement(object):
@@ -18,21 +19,33 @@ class Measurement(object):
         # Open the file and read
         file = open(source_path, 'r')
         line = file.readline()
+        count = 0
+
         while line != '':
+            count += 1
             if MeasurementRecord.is_header(line):
+                logger.debug('Read header line: %s', line)
                 record = MeasurementRecord(line)
 
                 # Now read data to current record
                 line = file.readline()
                 while line != '':
                     if MeasurementData.is_data(line):
+                        count += 1
                         record << line
+                        # Read next line
+                        line = file.readline()
                     else:
                         # Stop reading data
                         self.records.append(record)
                         break
+            elif line.startswith(chr(0x03)):
+                # End-of-text
+                file.close()
+                break
             else:
                 file.close()
+                logger.warning('Error in line %d', count)
                 raise MeasurementRecordError(line)
         file.close()
 
